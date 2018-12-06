07c070a9eeb007d33d377a15f7976e3d103ce2c7
mo watermelon <18508817+mowatermelon@users.noreply.github.com>
2018-12-06 21:26:37 +0800

    docs:modify method instructions
    docs:modify method instructions

--- basejs/sqlParser.js
+++ basejs/sqlParser.js
@@ -4,21 +4,24 @@
 String.prototype.trimAll = function () {
     return this && this.replace(/\s/g, '');
 };
+
 /**
  * 正常控制台日志输出
  * @param {String} msg 需要输出的内容
  */
-const log = (msg) => {
+const log = msg => {
     console.log(msg);
 };
+
 /**
  * 正常控制台断言输出
- * @param {String} condition 用作判断的等式
+ * @param {Boolean} condition 用作判断的等式
  * @param {String} msg 需要输出的内容
  */
 const assert = (condition, msg) => {
     console.assert(condition, msg);
 };
+
 /**
  * 将当前拼接字段的sql字符串分割成对应可以使用的数组
  * @param {String} fieldData 当前字段名
@@ -43,7 +46,7 @@ const getFormatField = fieldData => {
 
 /**
  * 将数据集字段字符串转化成对象
- * @param {Object} data 需要转化的数据集字段
+ * @param {String} data 需要转化的数据集字段
  */
 const splitSetField = data => {
     const errStr = '字段别名不允许重复,字段名与别名之间只能通过大写的AS进行分割';
@@ -109,11 +112,9 @@ const assertSqlParser = (testStrSql, assertLength) => {
 };
 
 /**
- * 添加字段字符串sql的测试案例
- * @param {String} testStrSql 用于检测的字段sql字符串
- * @param {Number} assertLength 用于断言将会出现多少个字段
+ * 实际开始检测sql解析
  */
-const testSqlParser = (() => {
+(() => {
     let rightStr01 = 'F1,F2,B,FF';
     let rightStr02 = 'F1 AS A,F2 AS B,FF AS C';
     let rightStr03 = "MD5_CHK( SELECT F0 FROM T0 WHERE C1<1),F2,NVL(F3,F4 ),(SELECT F5,F51,F52 FROM (SELECT F6 FROM (SELECT F61 FROM T10 WHERE C10=1) WHERE F7>1)  T1 WHERE  F8 ='1') AS C,TO_NUMBER(REGEXP_REPLACE('124ADC123','[^0-9]'))";
